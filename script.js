e180ee465b6a7dd5a462bf263924047f5ae5112a
sarath629 <124670726+sarath629@users.noreply.github.com>
2023-03-16 11:31:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 // complete the given function
 
 function palindrome(str){
+	str=str.toLowerCase();
  var s=0;
 	var end=str.length()-1;
 	while (s<end) {
+		if(str.charAt(s)==' '){
+			s++;
+		}
+		if(str.charAt(end)==''){
+			end--;
+		}
 		if(str.charAt(s)!=str.charAt(end)){
 			return false;
 			
