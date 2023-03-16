f9b48e2cf7bf88c4cf8952a5be020c6a0a705bf3
sarath629 <124670726+sarath629@users.noreply.github.com>
2023-03-16 11:23:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 // complete the given function
 
 function palindrome(str){
-
+ var s=0;
+	var end=str.length()-1;
+	while (s<end) {
+		if(str.charAt(s)!=str.charAt(end)){
+			return "false";
+			
+		}
+		s++;
+		end--;
+		
+	}
+	return "true";
 }
 module.exports = palindrome
